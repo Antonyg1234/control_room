ca0fa18acf2e3bc959c2419c2895a09a91605a23
amar.prajapati@neosofttech.com <amar.prajapati@neosofttech.com>
2020-04-01 20:53:42 +0530

    Worked on forgot password functionality.

--- app/Http/Controllers/API/RegisterController.php
+++ app/Http/Controllers/API/RegisterController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\User;
 use Illuminate\Support\Facades\Auth;
 use Validator;
+use Mail;
 
 class RegisterController extends BaseController
 {
@@ -54,5 +55,79 @@ class RegisterController extends BaseController
         else{
             return $this->sendError('Unauthorised.', ['error'=>'Unauthorised']);
         }
+    }
+
+    /**
+     * Login api
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function forgotPassword(Request $request)
+    {
+        if($request->email){
+            if($request->otp){
+                $validator = Validator::make($request->all(), [
+                    'email' => 'required|email',
+                    'otp' => 'required|number'
+                ]);
+    
+                if($validator->fails()){
+                    return $this->sendError('Validation Error.', $validator->errors());
+                }
+    
+                $otp_details = User::where([
+                    ['email', '=', $request->email],
+                    ['otp', '=', $request->otp]
+                ])->first();
+
+                if($otp_details){
+                    $otp_arr = [
+                        "otp" => null
+                    ];
+                   $update_otp = User::where('email', $request->email)->update($otp_arr);
+                   echo json_encode(array("success" => "Password generated successfully!"));
+                }else{
+                    return json_encode(array("msg" => "Invalid OTP!"));
+                }
+    
+            }
+            $validator = Validator::make($request->all(), [
+                'email' => 'required|email'
+            ]);
+
+            if($validator->fails()){
+                return $this->sendError('Validation Error.', $validator->errors());
+            }
+
+            $user_details = User::where('email', $request->email)->first();
+            if($user_details){
+                $email = array('email' => $user_details->email);
+                $otp = rand(0000,9999).$user_details->id;
+                $data = [
+                    "otp" => $otp
+                ];
+                $updated_user_details = User::where('id', $user_details->id)->update($data);
+
+                Mail::send('forgot_password_mail', $data, function($message) use($email) {
+                    $message->to($email['email'], 'testing')->subject
+                       ('MHADA Control Room Login');
+                    $message->from('noreply@gmail.com');
+                });
+                echo json_encode($user_details);
+            }
+        }
+
+        
+
+        // if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){
+        //     $user = Auth::user();
+        //     $success['token'] =  $user->createToken('MyApp')-> accessToken;
+        //     $success['name'] =  $user->name;
+
+        //     return $this->sendResponse($success, 'User login successfully.');
+        // }
+        // else{
+        //     return $this->sendError('Unauthorised.', ['error'=>'Unauthorised']);
+        // }
     }
 }
---
+++ resources/views/forgot_password_mail.blade.php
@@ -0,0 +1,1 @@
+<p>OTP is: $otp</p>
